d9b5f111737d6cd67ba3a9bab36d9f11c5ef0d4c
bi3mer <colan@comcast.net>
2023-01-05 11:03:01 -0500

    alpha-beta negamax

--- src/ai/alpha_beta.rs
+++ src/ai/alpha_beta.rs
@@ -18,7 +18,7 @@ impl AlphaBeta {
         AlphaBeta {}
     }
 
-    fn negamax(&mut self, board: &Board, depth: u8) -> f32 {
+    fn negamax(&mut self, board: &Board, depth: u8, alpha: f32, beta: f32) -> f32 {
         // Check if game is drawn
         if board.is_draw() {
             return 0.;
@@ -38,19 +38,27 @@ impl AlphaBeta {
                 return (F_WIDTH*F_HEIGHT+1. - (board.counter as f32)) / 2.;
             }
         }
+
+        // prune if beta is greater than the max possible score
+        let max = (F_WIDTH*F_HEIGHT- 1.0 - board.counter as f32)/2.;
+        let mut b = beta;
+        if beta > max {
+            b = max;
+            if alpha >= b { return b; }
+        }
         
         // Run negamax
         // https://en.wikipedia.org/wiki/Negamax#Negamax_with_alpha_beta_pruning
-        let mut score: f32 = -10000.;
+        // let mut score: f32 = -10000.;
+        let mut a = alpha;
         
         for next_board in boards.iter() {
-            let s = -self.negamax(next_board, depth - 1);
-            if s > score {
-                score = s;
-            }
+            let s = -self.negamax(next_board, depth - 1, -beta, -a);
+            if s >= b { return s; }
+            if s > a { a = s; }
         }
 
-        score
+        a
     }
 
     pub fn make_move(&mut self, board: &mut Board, max_depth: u8, ai_type: &AIType) {
@@ -59,8 +67,13 @@ impl AlphaBeta {
         let mut scores = Vec::new();
 
         for b in boards.iter() {
+            let mut s = -self.negamax(
+                b, 
+                max_depth, 
+                -(F_WIDTH*F_HEIGHT)/2.,
+                (F_WIDTH*F_HEIGHT)/2.
+            );
 
-            let mut s = -self.negamax(b, max_depth);
             if *ai_type != AIType::Hard {
                 s += rng.gen::<f32>()*0.;
             }
@@ -81,43 +94,3 @@ impl AlphaBeta {
         std::mem::swap(board, &mut (boards[index]));
     }
 }
-
-
- 
-        // if board.is_white_turn() {
-        //     // Minimize since it is the player's turn
-        //     score = 1000000000.;
-        //     for cur_board in boards.iter() {
-        //         let k = cur_board.hash();
-        //         if self.visited_states.contains_key(&k) {
-        //             score = score.min(*self.visited_states.get(&k).unwrap());
-        //         } else {
-        //             let board_score = self.ab(cur_board, depth - 1, a, b);
-        //             self.visited_states.insert(k, board_score);
-        //             score = score.min(board_score);
-
-        //             b = b.min(score);
-        //             if score <= alpha { break; }
-        //         }
-
-                
-        //     }
-        // } else {
-        //     // Maximize since it is the AI's turn
-        //     score = -1000000000.;
-        //     for cur_board in boards.iter() {
-        //         let k = cur_board.hash();
-        //         if self.visited_states.contains_key(&k) {
-        //             score = score.max(*self.visited_states.get(&k).unwrap());
-        //         } else {
-        //             let board_score = self.ab(cur_board, depth-1, a, b);
-        //             self.visited_states.insert(k, board_score);
-        //             score = score.max(board_score);
-
-        //             a = a.max(score);
-        //             if score >= beta { break; }
-        //         }
-        //     }
-        // }
-        // 
-        // score
