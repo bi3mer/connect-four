882c28ff6fa102d7facf82d0ec27a2c8d410a73c
bi3mer <colan@comcast.net>
2023-01-09 13:44:03 -0500

    beginner bot uses next non losing moves as well so it forces the player to be a bit smart

--- src/ai/random.rs
+++ src/ai/random.rs
@@ -4,7 +4,10 @@ use std::mem::swap;
 use crate::board::Board;
 
 pub fn make_move(board: &mut Board) {
-    let mut boards = board.get_next_boards();
+    let mut boards = board.get_next_non_losing_boards();
+    if boards.len() == 0 {
+        boards = board.get_next_boards();
+    }
     let mut rng = rand::thread_rng();
     let i = rng.gen_range(0..boards.len());
 
